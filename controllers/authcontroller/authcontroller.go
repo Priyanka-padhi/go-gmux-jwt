5ea7ae673cb1dbf7f213745f87ce832333d2b31a
Priyanka-padhi <prishi1212@gmail.com>
2022-10-06 22:28:24 +0530

    initial commit

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -1,11 +1,15 @@
 package authcontroller
 
-import (
+import  (
 	"encoding/json"
+	"github.com/golang-jwt/jwt/v4"
+	"go-gmux-jwt/config"
 	"go-gmux-jwt/helper"
 	"go-gmux-jwt/models"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -20,11 +24,60 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	//      data user      username
-	//var user models.User
-	//if err := models.DB.Where("username = ?",userInput.Username).First(&user).Error;
-	//
+	var user models.User
+	if err := models.DB.Where("username = ?",userInput.Username).First(&user).Error;
+	err != nil{
+		switch err {
+		case gorm.ErrRecordNotFound:
+			response := map[string]string{"message": "Username and password not exist"}
+			helper.ResponseJSON(w, http.StatusUnauthorized, response)
+			return
+		default:
+			response := map[string]string{"message": err.Error()}
+			helper.ResponseJSON(w, http.StatusInternalServerError, response)
+			return
+		}
+	}
+	//     password valid
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password).[]byte(userInput.Password));
+	err != nil {
+		response := map[string]string{"message": "Username bvb and password vbjkfn"}
+		helper.ResponseJSON(w, http.StatusUnauthorized, response)
+		return
+	}
+
+	//    token jwt
+    expTime := time.Now().Add(time.Minute * 1)
+	claims := &config.JWTClaim{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: "go-jwt-mux",
+			ExpiresAt: jwt.NewNumericDate(expTime),
+		},
+	}
+	//     algorithm    signing
+	tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+
+	//signed token
+	token, err := tokenAlgo.SignedString(config.JWT_KEY)
+	if err != nil{
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	//set token     cookie
+	http.SetCookie(w, &http.Cookie{
+		Name: "token",
+		Path: "/",
+		Value: token,
+		HttpOnly: true,
+	})
+	response := map[string]string{"message": "login Successful"}
+	helper.ResponseJSON(w, http.StatusOK, response)
 
 }
+
 func Register(w http.ResponseWriter, r *http.Request) {
 	//input json
 	var userInput models.User
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module go-gmux-jwt
 go 1.16
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.7 // indirect
-	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
+	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
 	gorm.io/driver/postgres v1.3.10
 	gorm.io/gorm v1.23.10
 )
